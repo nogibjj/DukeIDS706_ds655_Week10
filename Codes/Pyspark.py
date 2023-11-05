38042174c06e15042c0baed440831e9870b5f8ea
Divya Sharma <divya.sharma@duke.edu>
2023-11-05 07:18:12 +0000

    updated requirements

--- Codes/Pyspark.py
+++ Codes/Pyspark.py
@@ -1,22 +1,27 @@
-import urllib.request
+import pandas as pd
 from pyspark.sql import SparkSession
-from pyspark import SparkFiles
+import io
+import requests
+import gzip
 
-# Download the file
+# URL of the file to download
 url = "http://archive.ics.uci.edu/ml/machine-learning-databases/00280/HIGGS.csv.gz"
-filename = "HIGGS.csv.gz"
-urllib.request.urlretrieve(url, filename)
 
+# Create a SparkSession
 spark = SparkSession.builder.appName("UCI ML Repo data processing").getOrCreate()
-spark.sparkContext.addFile(url)
 
-df = spark.read.csv(SparkFiles.get("HIGGS.csv.gz"), header=True, inferSchema=True)
+# Download the file and decompress it
+r = requests.get(url)
+gz = gzip.open(io.BytesIO(r.content))
+df_pd = pd.read_csv(gz)
 
-# filtered_data = df.select("column_name").filter(df["column_name"] > 100)
+# Convert the pandas DataFrame to a PySpark DataFrame
+df = spark.createDataFrame(df_pd)
 
+# Count the number of rows in the DataFrame
 count = df.count()
 
 print("Number of rows in the dataset: ", count)
-# filtered_data.write.csv('s3a://bucket_name/path_to_save_file.csv')
+
+# Stop the SparkSession
 spark.stop()
-# os.remove("HIGGS.csv.gz")
